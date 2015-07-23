e1fbe91fc2d018959065ae72f13fd6b0d7111dd7
Robert Kardos <kardos.robert.kh@gmail.com>
2015-07-23 02:16:09 +0200

    Map is filled with random hallway blocks.

--- app.js
+++ app.js
@@ -2,11 +2,149 @@ var canvasElement = document.getElementById('myCanvas');
 
 var canvas = canvasElement.getContext('2d');
 
-function Map (width, height, blocksize) {
+/*function Room (width, height) {
 	this.width     = width;
 	this.height    = height;
-	this.blocksize = blocksize;
-	this.map       = [];
+	this.field = [];
+}
+Room.prototype.createRoom = function () {
+	for (var i = 0; i < this.width; i++) {
+		this.map[i] = [];
+		for (var j = 0; j < this.height; j++) {
+			this.map[i][j] = 0;
+		}
+	}
+};*/
+
+hallway = {
+	hallwayTypes : {
+		1 : [                   // 1  : horizontal
+			[0, 0, 0, 0, 0],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[0, 0, 0, 0, 0]
+		],
+		2 : [                   // 2  : vertical
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0]
+		],
+		3 : [                   // 3  : up right turn
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 0, 0, 0, 0]
+		],
+		4 : [                   // 4  : right down turn
+			[0, 0, 0, 0, 0],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 0]
+		],
+		5 : [                   // 5  : down left turn
+			[0, 0, 0, 0, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0]
+		],
+		6 : [                   // 6  : left up turn
+			[0, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[0, 0, 0, 0, 0]
+		],
+		7 : [                   // 7  : up fork
+			[0, 1, 1, 1, 0],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[0, 0, 0, 0, 0]
+		],
+		8 : [                   // 8  : right fork
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 0]
+		],
+		9 : [                   // 9  : down fork
+			[0, 0, 0, 0, 0],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[0, 1, 1, 1, 0]
+		],
+		10 : [                  // 10 : left fork
+			[0, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0]
+		],
+		11 : [                  // 11 : up dead end
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 0, 0, 0, 0]
+		],
+		12 : [                  // 12 : right dead end
+			[0, 0, 0, 0, 0],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 1, 1, 1, 1],
+			[0, 0, 0, 0, 0]
+		],
+		13 : [                  // 13 : down dead end
+			[0, 0, 0, 0, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0],
+			[0, 1, 1, 1, 0]
+		],
+		14 : [                  // 14 : left dead end
+			[0, 0, 0, 0, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[1, 1, 1, 1, 0],
+			[0, 0, 0, 0, 0]
+		],
+		15 : [                  // 15 : cross
+			[0, 1, 1, 1, 0],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[1, 1, 1, 1, 1],
+			[0, 1, 1, 1, 0]
+		]
+	},
+	drawHallway : function (x, y, type, magnification) {
+		for (var i = 0; i < this.hallwayTypes[type].length; i++) {
+			for (var j = 0; j < this.hallwayTypes[type].length; j++) {
+				if (hallway.hallwayTypes[type][i][j] == 1) {
+					canvas.fillStyle = 'rgb(230, 230, 230)';
+					canvas.fillRect(x + i * 10, y + j * 10, 10, 10);
+				} else {
+					canvas.fillStyle = 'rgb(30, 30, 30)';
+					canvas.fillRect(x + i * 10, y + j * 10, 10, 10);
+				}
+			}
+		}
+	}
+};
+
+
+function Map (width, height, hallwaysize) {
+	this.width       = width;
+	this.height      = height;
+	this.hallwaysize = hallwaysize;
+	this.map         = [];
 
 }
 
@@ -20,109 +158,21 @@ Map.prototype.generateMap = function () {
 };
 
 Map.prototype.drawMap = function () {
-	/*for (var i = 0; i < this.width; i++) {
-		for (var j = 0; j < this.height; j++) {
-			if (this.map[i][j] == 0) {
-				canvas.fillStyle = 'rgb(30, 30, 30)';
-			} else {
-				canvas.fillStyle = 'rgb(230, 230, 230)';
-			}
-			canvas.fillRect(i * this.blocksize, j * this.blocksize, this.blocksize, this.blocksize);
+	var that = this,
+		x    = 0 * this.hallwaysize,
+		y    = 0 * this.hallwaysize,
+		hallwayType;
+
+	/*hallway.drawHallway(x, y, hallwayType, that.hallwaysize);*/
+
+
+	for (var i = 0; i < that.width; i++) {
+		for (var j = 0; j < that.height; j++) {
+			hallwayType = Math.round(Math.random() * 14 + 1);
+			that.map[i][j] = hallwayType;
+			hallway.drawHallway(i * this.hallwaysize, j * this.hallwaysize, hallwayType, that.hallwaysize);
 		}
-	}*/
-
-	canvas.fillStyle = 'rgb(30, 30, 30)';
-
-	var that      = this,
-		x         = 0,
-		y         = this.height - 1,
-		directionProblem = [];
-
-
-	var count = 0;
-
-
-	var stepAtRandom = function () {
-		var possibleDirections = [0, 1, 2, 3];
-
-		var direction;
-		/*do {
-			direction = randomizeDirection(possibleDirections);
-			
-		}
-		while (true);*/
-
-
-		do {
-			direction = randomizeDirection(possibleDirections);
-			if (direction !== -1) {
-				if (checkDirection(possibleDirections[direction])) {
-					possibleDirections = [0, 1, 2, 3];
-					canvas.fillStyle = 'rgb(' + count * 2 + ', ' + count * 2 + ', ' + count * 2 + ')';
-					that.map[x][y] = 1;
-					count++;
-					canvas.fillRect(x * that.blocksize, y * that.blocksize, that.blocksize, that.blocksize);
-				} else {
-					possibleDirections.splice(direction, 1);
-				}
-			}
-			console.log(count);
-		}
-		while (possibleDirections.length != 0);
-
-		for (var i = 0; i < that.width; i++) {
-			for (var j = 0; j < that.height; j++) {
-				/*console.log(that.map[i][j]);*/
-			}
-		}
-
-	};
-
-
-
-	var checkDirection = function (direction) {
-
-
-		switch (direction) {
-			case 0:
-				if (y === 0 || that.map[x][y - 1]) {
-					return false;
-				}
-				y -= 1;
-			break;
-			case 1:
-				if (x == that.width - 1 || that.map[x + 1][y]) {
-					return false;
-				}
-				x += 1;
-			break;
-			case 2:
-				if (y == that.height - 1 || that.map[x][y + 1]) {
-					return false;
-				}
-				y += 1;
-			break;
-			case 3:
-				if (x === 0 || that.map[x - 1][y]) {
-					return false;
-				}
-				x -= 1;
-			break;
-		}
-
-		return true;
-	};
-
-	var randomizeDirection = function (possibleDirections) {
-		if (possibleDirections.length !== 0) {
-			return possibleDirections[Math.round(Math.random() * (possibleDirections.length - 1))];
-		}
-		return -1;
-	};
-
-
-	stepAtRandom();
-
+	}
 };
 
 var map = new Map(10, 10, 50);
