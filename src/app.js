60cc165500ca84153815239a2596191e87ec9b9a
Robert Kardos <robi@clipdis.com>
2016-06-28 01:17:11 +0200

    Port to Webpack (#1)
    rewrite to Webpack and ES6
    
    The entire project has been casually rewritten to ES6 and Webpack, in a
    single, lean, absolutely not bloated commit.
    Also, it has been modularized a bit.

---
+++ src/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import * as _ from 'underscore';
+import { Game } from './game';
+import { Labyrinth } from './labyrinth';
+
+let labyrinthConfig = {
+	width          : 10,
+	height         : 10,
+	// wallThickness  : 1, // default : 10
+	size           : 5,
+	// step           : 10,
+	randomness     : 4,
+	write          : false,
+	ereaseDeadEnds : 0,
+	rooms          : {
+		roomAttempts   : 100,
+		pRoomWidthMax  : 3,
+		pRoomWidthMin  : 1,
+		pRoomHeightMax : 3,
+		pRoomHeightMin : 1
+	}
+};
+
+let labyrinth = new Labyrinth(labyrinthConfig);
+
+var game = new Game(labyrinth);
+// game.start();
